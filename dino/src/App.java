bf2de08d3e8b2265342206b374e1d81727625412
NY-ANDRY <155851768+NY-ANDRY@users.noreply.github.com>
2024-12-20 21:56:06 +0300

    Delete dino directory

--- dino/src/App.java
+++
@@ -1,9 +0,0 @@
-import aff.Fenetre;
-// import com.formdev.flatlaf.themes.FlatMacLightLaf;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        // FlatMacLightLaf.setup();
-        new Fenetre();
-    }
-}
